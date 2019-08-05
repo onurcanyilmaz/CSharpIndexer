2ece8b948f8111517d3b18c1d3bc16f6b137c0e9
onurcanyilmaz <oyilmaz1630@gmail.com>
2019-08-05 23:05:06 +0300

    initial Commit

---
+++ CsharpIndexer/Program.cs
@@ -0,0 +1,49 @@
+﻿using CsharpIndexer.src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpIndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test T = new Test();
+
+
+            Console.WriteLine("Enter number..  ");
+            var key = Console.ReadLine();
+
+            //out n variable 
+            var isNumeric = int.TryParse(key, out int n);
+
+            // define this method 
+            // Another(n);
+
+            if (isNumeric)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    T[i] = i;
+                }
+            }
+
+            else
+                Console.WriteLine("Please Enter The Number");
+        }
+
+        //   another usage 
+        static void Another(int count)
+        {
+            Test T = new Test(count);
+
+            for (int i = 0; i < count; i++)
+            {
+                T[i] = i;
+            }
+        }
+    }
+}
---
+++ CsharpIndexer/src/Test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpIndexer.src
+{
+    public class Test
+    {
+        private int _count;
+        private int[] numbers;
+
+        public Test(int count)
+        {
+            _count = count;
+            numbers = new int[_count];
+        }
+
+        public Test()
+        {
+            this._count = 1;
+            numbers = new int[_count];
+        }
+
+
+        #region Indexer
+
+        public int this[int index]
+        {
+            get
+            {
+                return numbers[index];
+            }
+            set
+            {
+                numbers[index] = value;
+            }
+        }
+
+        #endregion
+    }
+}
